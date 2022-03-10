9e1785df40b67c1a3f51db4b91dc73b101c2cd5f
Kiley <github@kileymitti.com>
2022-03-10 12:59:47 -0600

    add redirects file for netlify

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,38 +15,6 @@ const Home = () => {
       <Hero />
       <Resume />
 
-      <div id='features'>
-        <AiFillStar className='icon' />
-        <h2>Our Features</h2>
-        <BsFillCloudRainFill className='icon' />
-        <h3>We&rsquo;ll Watch the Weather</h3>
-        <p>
-          Download our app and we&rsquo;ll send you a notice if it&rsquo;s about
-          to rain in the next 20 minutes around your current location. A good
-          rain can be refreshing, but sometimes you just want to stay dry.
-        </p>
-        <BsGlobe2 className='icon' />
-        <h3>Global Guides</h3>
-        <p>
-          We&rsquo;ve scoured the entire planet for the best retreats and
-          beautiful vistas. If there&rsquo;s a corner of the world you want to
-          escape to we know the most scenic and inspiring locations.
-        </p>
-        <BsWifi className='icon' />
-        <h3>Wi-Fi Waypoints</h3>
-        <p>
-          We only send you on trips to places we can personally vouch for as
-          being amazing. Which means we&rsquo;ve mapped out where local wi-fi
-          spots are and marked them in our app&rsquo;s map view.
-        </p>
-        <AiTwotoneFire className='icon' />
-        <h3>Survival Kit</h3>
-        <p>
-          Everytime you book an escape with us we send you a survival kit with
-          the finest materials. The kit will allow you to setup a tent, start a
-          fire, scratch your own back and lower your taxes.
-        </p>
-      </div>
       <div id='testimonials'>
         <AiOutlineComment className='icon' />
         <h2>Real Testimonials</h2>
